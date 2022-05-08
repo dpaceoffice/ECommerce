ba09c60e29b13aefaace0ade7d8ab80ea2d64e91
dpaceoffice <dpaceoffice@gmail.com>
2022-05-08 17:04:21 -0500

    mongo refractoring for store model #1

--- server/models/Store.js
+++ server/models/Store.js
@@ -2,105 +2,31 @@ import Product from "./Product.js";
 import Category from "./Category.js";
 import Cart from "./Cart.js";
 import Customer from "./Customer.js";
+import mongoose from "mongoose";
 
+const Schema = mongoose.Schema;
+const storeSchema = new Schema({
+  products: [{ type: Schema.ObjectId, ref: 'Product' }],
+  categories: [{ type: Schema.ObjectId, ref: 'Category' }],
+  customers: [{ type: Schema.ObjectId, ref: 'Customer' }],
+})
 /**
  * Store Object
  */
-export default class Store {
-  #curCtg = -1; //int current category id being viewed
-  #categories = {}; //dictionary of categories
-  #products = new Map(); //map of products
-  #customer; //Customer object for the customer
-  /**
-   * Initilizes the store
-   */
-  constructor() {
-    var products = {};
-    let katana = new Product(
-      0,
-      "Katana",
-      "A very very sharp sword used by samurai",
-      5.0,
-      "./assets/katana.png"
-    );
-    products[katana.getID()] = katana;
-    this.#products.set(katana.getID(), katana);
-
-    let gs = new Product(
-      1,
-      "Great Sword",
-      "A great sword, used by the largest men",
-      25.0,
-      "./assets/greatsword.png"
-    );
-    products[gs.getID()] = gs;
-    this.#products.set(gs.getID(), gs);
-
-    let cat = new Category(0, "Swords", products);
-    this.#categories[cat.getID()] = cat;
-
-    products = {};
-
-    let crossbow = new Product(
-      2,
-      "Crossbow",
-      "A ranged weapon using an elastic launching device consisting of a bow-like assembly called a prod, mounted horizontally on a main frame called a tiller",
-      3.3,
-      "./assets/crossbow.png"
-    );
-    products[crossbow.getID()] = crossbow;
-    this.#products.set(crossbow.getID(), crossbow);
-
-    let longbow = new Product(
-      3,
-      "Longbow",
-      "A type of tall bow that makes a fairly long draw possible",
-      25.0,
-      "./assets/longbow.png"
-    );
-    products[longbow.getID()] = longbow;
-    this.#products.set(longbow.getID(), longbow);
-
-    let cat2 = new Category(1, "Bows", products);
-    this.#categories[cat2.getID()] = cat2;
-    let cat3 = new Category(2, "Axes", {});
-    this.#categories[cat3.getID()] = cat3;
-    this.#curCtg = 0;
-
-    let cart = new Cart(0);
-    let cartarr = [];
-    cartarr.push(cart);
-    let customer = new Customer({ name: "David", email: "Whatever", carts: cartarr, curCart: 0 });
-    this.#customer = customer;
-  }
+class Store {
   /**
    *
    * @returns the customer object
    */
-  getCustomer() {
-    return this.#customer;
+  getCustomers() {
+    return this.customers;
   }
   /**
    *
    * @returns all categories
    */
   getCategories() {
-    return this.#categories;
-  }
-  /**
-   *
-   * @returns current viewed category ID
-   */
-  getCurrentCategory() {
-    return this.#curCtg;
-  }
-  /**
-   * Sets the current category to int
-   * @param {int} id
-   *
-   */
-  setCurrentCategory(id) {
-    this.#curCtg = id;
+    return this.categories;
   }
   /**
    *
@@ -108,10 +34,53 @@ export default class Store {
    * @returns project object from id
    */
   getProduct(id) {
-    return this.#products.get(id);
+    return this.products[id];
+  }
+  /**
+   * 
+   * @returns product ids in store
+   */
+  allProducts() {
+    return this.products;
   }
 
+  async addProduct(product) {
+    if (!(this.products.includes(product['_id'])))
+      this.products.push(product);
+    return await this.save();
+  }
+  async addCategory(category) {
+    if (!(this.categories.includes(category['_id'])))
+      this.categories.push(category);
+    return await this.save();
+  }
+  async createProduct(title, description, price, image) {
+    const exists = await Product.find({ title: title });
+    if (exists.length > 0) {
+      return exists[0];
+    }
+    let newProduct = new Product({
+      title: title,
+      des: description,
+      price: price,
+      image: image
+    });
+    return await newProduct.save();
+  }
+  async createCategory(type, products) {
+    const exists = await Category.find({ type: type });
+    if (exists.length > 0) {
+      return exists[0];
+    }
+    console.log("New Category: " + type);
+    let newCategory = new Category({
+      type: type,
+      products: products
+    });
+    return await newCategory.save();
+  }
 }
 
-
+storeSchema.loadClass(Store);
+export default mongoose.model('Store', storeSchema);
 
