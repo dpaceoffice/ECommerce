eb8c2d4bc33308db883e33df60b6a90e71b35d28
dpaceoffice <dpaceoffice@gmail.com>
2022-05-12 15:47:12 -0500

    Passport middleware #4

---
+++ server/config/passport.js
@@ -0,0 +1,28 @@
+import { Strategy } from "passport-local";
+import passport from "passport";
+import Store from "./models/Store.js";
+
+async function authenticateUser(email, password, done) {
+  const user = await Store.findUser("email", email);
+  if (user === undefined) {
+    console.log("No user with that email");
+    return done(null, false);
+  }
+  if (password === user.password) {
+    console.log("User Authenticated");
+    return done(null, user);
+  } else {
+    console.log("Password incorrect");
+    return done(null, false);
+  }
+}
+
+async function setupPassport() {
+    const formNames = { usernameField:'email', passwordField:'password' };
+    const localStrategy = new Strategy( formNames, authenticateUser);
+    passport.use(localStrategy);
+    passport.serializeUser( (user, done) => done(null, user['_id']) );
+    passport.deserializeUser( (id,done) => done(null, await Store.serializeCustomer(id)) );
+}
+setupPassport();
+export default passport;
