52b2ea82e6866a64911043c58b957bb3077802ea
ljgilm <78664360+ljgilm@users.noreply.github.com>
2022-05-15 00:02:22 -0500

    Admin - removed extra code that was from lab #13

--- server/app.js
+++ server/app.js
@@ -22,21 +22,11 @@ else {
 const app = express();
 const port = process.env.PORT | 3000;
 
-/* ADMINISTRATION CODE */
-function localhostHandler(request, response, next) {
-    response.header('Access-Control-Allow-Origin', '*');
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    response.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    next();
-}
-/*^^^^ ADMINISTRATION CODE ^^^^*/
-
 const routes = new Router().getRouter();
 function setupApp() {
     app.use(cors());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }))     //use body parser to read url 
-    app.use(localhostHandler);
     app.use('/', routes);
 }
 setupApp();
