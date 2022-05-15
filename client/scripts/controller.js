0c3c0a41034eb7c622baf315c4ad670e997ed8dd
ljgilm <78664360+ljgilm@users.noreply.github.com>
2022-05-14 23:58:13 -0500

    Refactored Admin - Created Callbacks Controller.js #13

--- client/scripts/controller.js
+++ client/scripts/controller.js
@@ -12,126 +12,29 @@ const addController = function (...buttonIDs) {
     }
 }
 
-/* ADMINISTRATION CODE */
 
-async function postLayout(order) {
-    const url = 'http://localhost:3000/admin/rearrangeLayout';
-    return await fetch(url,
-        {
-            method: 'POST',
-            body: order,
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            }
+const getAdminCallbacks = () => ({
+    "submitProduct": submitEvent,
+    "removeProdBut": postRemove,
+    "saveOrder": postLayout,
+    "wantToEditBut": editProductGetID,
+    "editProductBut": editEvent
+})
+
+const adminAddController = async function (...adminButtonIDs) {
+    const adminCallbacks = getAdminCallbacks();
+    for (let id of adminButtonIDs) {
+        for (let element of document.querySelectorAll('[id=' + id + ']')) {
+            console.log("ID: " + id);
+            element.addEventListener("click", () => {
+                console.log("CLICKED! "+ id);
+                if (id == "saveOrder") {
+                    console.log("LAYOUT___:" + window.localStorage.getItem('prodIdlayout'))
+                    adminCallbacks[id](window.localStorage.getItem('prodIdlayout'))
+                } else {
+                    adminCallbacks[id](element.attributes)
+                }
+            });
         }
-    )
-        .then(checkStatus)
-        .then(() => { console.log('updated!!!'); console.log(order) })
-        .then(() => { reloadAdminPage(); console.log("reload"); })
-}
-
-
-async function postRemove(e) {
-    const url = 'http://localhost:3000/admin/removeProduct';
-    const postData = await removeOldProduct(e);
-    await console.log(postData + " is post data!!!!!!!!!!");
-    return await fetch(url,
-        {
-            method: 'POST',
-            body: JSON.stringify({ id: postData }),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            }
-        }
-    )
-        .then(checkStatus)
-        .then(() => { console.log('updated!!!'); console.log(JSON.stringify({ id: postData })) })
-        .then(() => { reloadAdminPage(); console.log("reload"); })
-}
-
-async function postNewProduct(postData) {
-    const url = 'http://localhost:3000/admin/addProduct';
-    array = [postData];
-    return await fetch(url,
-        {
-            method: 'POST',
-            body: JSON.stringify(postData),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        }
-    )
-        .then(checkStatus)
-        .then(() => { console.log('updated!!!'); console.log(array[0]) })
-        .then(() => { reloadAdminPage(); console.log("reload"); })
-    //.then(() => { addNewProduct(array) })
-}
-
-async function postEditProduct(postData) {
-    const url = 'http://localhost:3000/admin/editProduct';
-    array = [postData];
-    return await fetch(url,
-        {
-            method: 'POST',
-            body: JSON.stringify(postData),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        }
-    )
-        .then(checkStatus)
-        .then(() => { console.log('editted update!!!'); console.log(array[0]) })
-        .then(() => { reloadAdminPage(); console.log("reload"); })
-}
-
-
-// https://hartzis.me/fetch-post-express/
-// https://medium.com/geekculture/how-to-send-forms-data-with-fetch-using-get-post-put-delete-and-catching-with-express-js-bfdb85b99709 
-
-function checkStatus(response) {
-    if (response.status >= 200 && response.status < 300) {
-        return response
-    } else {
-        var error = new Error(response.statusText)
-        error.response = response
-        throw error
     }
 }
-
-// 
-async function loadAdminPage() {
-    const url = 'http://localhost:3000/admin';
-    const response = await fetch(url);
-    const data = await response.json();
-    if (data) {
-        console.log("SUCCESS!");
-        console.log(data);
-        //startPage();
-        localStorage.clear();
-        adminPage(data);
-    } else {
-        console.log("FAIL!");
-    }
-    //console.log(data);
-    //return data;
-}
-
-async function getProductById(id) {
-    const url = `http://localhost:3000/productById/${id}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    if (data) {
-        console.log("SUCCESS!");
-        console.log(data);
-    } else {
-        console.log("FAIL!");
-    }
-    //console.log(data);
-    return data;
-}
-
-/*^^^^ ADMINISTRATION CODE ^^^^^*/
