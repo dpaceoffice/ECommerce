b48eb13f817d8dac2d34daea6cd4d0e0a88641ee
dpaceoffice <dpaceoffice@gmail.com>
2022-05-15 16:39:55 -0500

    changes needed for static store

--- server/config/default-controller.js
+++ server/config/default-controller.js
@@ -6,16 +6,9 @@ import passport from "./passport.js";
 
 export default class Controller {
     constructor() {
-        this.getStoreFront = this.getStoreData.bind(this);
-        this.setCategory = this.setCategory.bind(this);
-        this.getCategory = this.getCategory.bind(this);
-        this.getCheckout = this.getCheckout.bind(this);
-        this.addToCart = this.addToCart.bind(this);
-        this.removeFromCart = this.removeFromCart.bind(this);
-        this.getCarts = this.getCarts.bind(this);
-        this.test = this.test.bind(this);
-        //this.getStore = this.getStore.bind(this);
+
     }
+    /*
     async getStore() {
         var store = await Store.find({});
         if (store.length <= 0) {
@@ -24,7 +17,7 @@ export default class Controller {
             var model = store[0];
             return model;
         }
-    }
+    }*/
 
     getIndex(request, response) {
         response.json({ success: true });
@@ -48,8 +41,12 @@ export default class Controller {
     async getStoreData(request, response) {
         var cstate = await Category.find({}, { _id: 1, type: 1, products: 1 });
         var pstate = await Product.find({}, { _id: 1, title: 1, price: 1, des: 1, image: 1 });
-
-        response.json({ cstate, pstate });
+        const auth_user = request.user;
+        let user = undefined;
+        if (auth_user != undefined) {
+            user = auth_user.email;
+        }
+        response.json({ cstate, pstate, user });
     }
 
     getCheckout(request, response) {
@@ -74,15 +71,16 @@ export default class Controller {
     }
     login(request, response, next) {
         const { email, password } = request.body;
-        console.log(email, password);
+        let session = request.session.id;
+        console.log(request.body);
         console.log(request.session.id);//express
 
-        const config = {};
+        //const config = {};
         //config.successRedirect = '/';
         //config.failureRedirect = '/login';
-        const authHandler = passport.authenticate('local', config);
-        authHandler(request, response, next);//passes body into passport
-        //response.send('success');
+        //const authHandler = passport.authenticate('local', config);
+        //authHandler(request, response, next);//passes body into passport
+        response.json({ email, password, session });
     }
 
     getCarts(request, response) {
@@ -92,54 +90,50 @@ export default class Controller {
         response.json(data)
     }
     async test(request, response) {
-        var store = await this.getStore();
-
-        let katana = await store.createProduct(
+        await Store.createCustomer(
+            "David",
+            "david@test",
+            "password",
+            2
+        )
+        const authenticated_user = request.user;
+        if (authenticated_user == undefined || authenticated_user.rights < 2) {
+            return response.sendStatus(403);
+        }
+        let katana = await Store.createProduct(
             "Katana",
             "A very very sharp sword used by samurai",
             5.5,
             "./assets/katana.png"
         )
-        let gs = await store.createProduct(
+        let gs = await Store.createProduct(
             "Great Sword",
             "A great sword, used by the largest men",
             25.5,
             "./assets/greatsword.png"
         )
         let swords = [katana, gs];
-        let sword_cat = await store.createCategory("Swords", swords);
-        await store.addCategory(sword_cat);
+        await Store.createCategory("Swords", swords);
 
-        let crossbow = await store.createProduct(
+        let crossbow = await Store.createProduct(
             "Crossbow",
             "A ranged weapon using an elastic launching device consisting of a bow-like assembly called a prod, mounted horizontally on a main frame called a tiller",
             53.5,
             "./assets/crossbow.png"
         )
-        let longbow = await store.createProduct(
+        let longbow = await Store.createProduct(
             "Longbow",
             "A type of tall bow that makes a fairly long draw possible",
             25.0,
             "./assets/longbow.png"
         )
         let bows = [crossbow, longbow];
-        let bow_cat = await store.createCategory("Bows", bows);
-        await store.addCategory(bow_cat);
-        await store.createCustomer(
-            "David",
-            "david@test",
-            "password",
-            undefined,
-            0,
-            0,
-            undefined
-        )
-        const store_state = await Store.find();
+        await Store.createCategory("Bows", bows);
+
         const products = await Product.find();//persistent product scheme instances
         const categories = await Category.find();
         const users = await Customer.find();
-        const authenticated_user = request.user;
-        const data = { store_state, products, categories, users, authenticated_user };
+        const data = { products, categories, users, authenticated_user };
         response.json(data);
     }
 }
