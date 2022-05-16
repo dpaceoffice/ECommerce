f6a9c33ce7c449816c35340c1369c58ae79f80f2
ljgilm <78664360+ljgilm@users.noreply.github.com>
2022-05-16 13:22:03 -0500

    Admin Category - add, edit, remove, layout, and create cat (def-controller.js) #13
    can add, remove, edit products by category
    can save layout by category
    can create new category

--- server/controllers/default-controller.js
+++ server/controllers/default-controller.js
@@ -122,9 +122,10 @@ export default class Controller {
     }
 
     /* ADMINISTRATION CODE*/
-
     // SERVER NEEDS TO ACCESS MONGODB TO MAKE CHANGES TO THE DATABASE
 
+
+    // return list of categories ids and types
     getCategoriesIds(request, response) {
         const productsMongo = async () => {
             ;
@@ -136,100 +137,72 @@ export default class Controller {
         productsMongo();
     }
 
-    returnCatProducts(request, response) {
-        //const id = request.params.id;
-        //var store = await this.getStore();
+
+    // Receives category id and returns products in that category
+    async returnCatProducts(request, response) {
         console.log(request.params);
         console.log("ID: " + request.params.id + " | Type: " + request.params.type);
         const id = request.params.id;
         const type = request.params.type;
         var products = new Array();
+        // if creating a new category
         if (id == "-1") {
-            //await store.createCategory(type, []);
-            response.send(JSON.stringify({"id": "-1"},{"products": []}));
+            var store = await this.getStore();
+            const newCategory = await store.createCategory(type, []);
+            console.log(newCategory);
+            response.send(JSON.stringify({ "id": newCategory }, { "products": [] }));
         } else {
             let productsNum;
             let i = 0;
             let array = new Array();
-            const productsMongo = async () => {
-                //console.log(id);
-                //console.log(typeof id);
-                const categories = await Category.find({ _id: id }, { products: 1 });
-                //console.log(categories);
-                productsNum = await categories[0].products.length;
-                i = 0;
-                array = await categories[0].products;
-            };
+            const categories = await Category.find({ _id: id }, { products: 1 });
+            productsNum = await categories[0].products.length;
+            console.log("Product Numbers: ");
+            console.log(productsNum);
 
-            let mongoCatProd = function () {
-                return Category.find({ _id: id }, { products: 1 })
-            }
-            let mongoCatProd2 = mongoCatProd();
-            console.log(mongoCatProd);
-
-            mongoCatProd2.then(function(result) {
-                //console.log("what?????");
-                console.log(result[0].products);
-                productsNum = result[0].products.length;
-                console.log(catProdIds(result[0].products));
-                
-            })
-
-            const catProdIds = async (array2) => {
-                for (let element of array2) {
-                    //console.log(element.valueOf())    
-                    let prodID = element.toString();
-                    //console.log(typeof prodID + ": " + prodID);
-                    let addProd = function () { return Product.find({ _id: prodID })};
-                    let addProd2 = addProd();
-                    console.group(addProd);
-
-                    addProd2.then(function(result) {
-                        products.push(result[0]);
-                        i++;
-                        if (i == productsNum) {
-                            //console.log("product-------"+ i +" " + products);
-                            //console.log(typeof products)
-                            //response.send(JSON.stringify({product:products}));
-                            response.json(JSON.stringify({"products": products}));
-                        }
-                        //console.log("OUTSIDE-"+ products);
-                    })
-                    
+            // if category exists but is empty
+            if (productsNum == 0) {
+                response.json(JSON.stringify({"result":[{"products": []},{"id": id}]}))
+            } else {
+                // if category exists but is not empty
+                let mongoCatProd = function () {
+                    return Category.find({ _id: id }, { products: 1 })
                 }
-            }
-
-                /*
-                categories[0].products.forEach( async (element) => {
-                    //console.log(element.valueOf())    
-                    let prodID = element.toString();
-                    //console.log(typeof prodID + ": " + prodID);
-                    let addProd = await Product.find({ _id: prodID });
-                    //console.log(addProd[0]);
-                    products.push(await addProd);
-                    //console.log("product-"+ products);
-                });*/
-            /*const addProd = async (array2) => {
-                for (let element of array2) {
-                    //console.log(element.valueOf())    
-                    let prodID = element.toString();
-                    //console.log(typeof prodID + ": " + prodID);
-                    let addProd = await Product.find({ _id: prodID });
-                    //console.log(addProd[0]);
-                    products.push(await addProd);
-                    i++;
-                    if (i == productsNum) {
-                        console.log("product-"+ products);
-                        response.send(products);
+                let mongoCatProd2 = mongoCatProd();
+                console.log(mongoCatProd);
+    
+                mongoCatProd2.then(function (result) {
+                    //console.log("what?????");
+                    console.log(result[0].products);
+                    productsNum = result[0].products.length;
+                    console.log(catProdIds(result[0].products));
+                })
+                const catProdIds = async (array2) => {
+                    for (let element of array2) {
+                        //console.log(element.valueOf())    
+                        let prodID = element.toString();
+                        //console.log(typeof prodID + ": " + prodID);
+                        let addProd = function () { return Product.find({ _id: prodID }) };
+                        let addProd2 = addProd();
+                        console.log(addProd);
+                        addProd2.then(function (result) {
+                            products.push(result[0]);
+                            i++;
+                            if (i == productsNum) {
+                                console.log("Getting Category Products: " + id);
+                                console.log("Products are ");
+                                console.log(products);
+                                response.json(JSON.stringify({"result":[{"products": products},{"id": id}]}))
+                            }
+                            //console.log("OUTSIDE-"+ products);
+                        })
                     }
-                    
                 }
-            }*/
+            }
 
-            //console.log(productsMongo());
-            //console.group(addProd());
             
-        
+
+
         }
     }
 
@@ -238,170 +211,116 @@ export default class Controller {
         let result;
         const productsMongo = async () => {
             result = await Product.find();
-            //console.log(result);
-            //const categories = await Category.find({}, {products: 1});
-            //console.log(categories);
             response.json(result);
         };
         productsMongo();
 
-        let products = [
-            {
-                id: 10,
-                title: "Katana",
-                description: "A very very sharp sword used by samurai",
-                price: 5.0,
-                image: "./assets/katana.png"
-            }, {
-                id: 11,
-                title: "Great Sword",
-                description: "A great sword, used by the largest men",
-                price: 25.0,
-                image: "./assets/greatsword.png"
-            },
-            {
-                id: 12,
-                title: "Katana",
-                description: "A very very sharp sword used by samurai during the 12th century",
-                price: 5.0,
-                image: "./assets/katana.png"
-            },
-            {
-                id: 13,
-                title: "Great Sword",
-                description: "A great sword, used by the largest men",
-                price: 25.0,
-                image: "./assets/greatsword.png"
-            },
-            {
-                id: 14,
-                title: "Katana",
-                description: "A very very sharp sword used by samurai",
-                price: 5.0,
-                image: "./assets/katana.png"
-            },
-            {
-                id: 15,
-                title: "Great Sword",
-                description: "A great sword, used by the largest men",
-                price: 25.0,
-                image: "./assets/greatsword.png"
-            }
-
-        ]
-        //console.log("ARRAY");
-        //console.log(products);
-
-        //response.json(products);
-
     }
-
-    /* Add new product object to mongodb by receiving the new product as JSON
-    addProduct(request, response) {
-        response.send(request.body);
-        console.log("POST SUCCESS! Added product! Can update mongo!");
-        console.log(request.body);
-
-        let products = {
-            0: {
-                id: 0,
-                title: "Katana",
-                description: "A very very sharp sword used by samurai",
-                price: 5.0,
-                image: "./assets/katana.png"
-            },
-            1: {
-                id: 1,
-                title: "Great Sword",
-                description: "A great sword, used by the largest men",
-                price: 25.0,
-                image: "./assets/greatsword.png"
-            },
-        }
-        products[Number(request.body.id)] = request.body;
-        //console.log("APPENDED");
-        //console.log(products);
-    }*/
-
 
     /* Add new product object to mongodb by receiving the new product as JSON */
     async addProduct(request, response) {
         response.send(request.body);
         console.log("POST SUCCESS! Added product! Can update mongo!");
+        console.log("Category: " + request.body[1])
+        console.log("PRODUCT: -----------------");
+        console.log(request.body[0]);
+
+        var store = await this.getStore();
+
+        var product = await store.createProduct(
+            request.body[0].title,
+            request.body[0].description,
+            request.body[0].price,
+            request.body[0].image
+        )
+
+        console.log("Product : -----" + product);
+        await Category.updateOne(
+            { _id: request.body[1] },
+            { $push: { products: product } }
+        )
+
+
+    }
+
+    /* Can rearrange layout/order of products by sending an array that contains the order based on the products' id */
+    async rearrangeLayout(request, response) {
+        response.send(request.body);
+        console.log("POST SUCCESS! Rearrange Layout! Can update mongo!");
+        console.log(request.body);
+        console.log(typeof request.body.order)
+        console.log(request.body.order);
+        console.log(typeof request.body.curCat)
+        console.log(request.body.curCat);
+        console.log("inside array");
+        console.log(request.body.order[0]);
+        console.log(request.body.order[1]);
+
+        let layout = await Category.updateOne(
+            { _id: request.body.curCat },
+            {
+                $set:
+                    { products: request.body.order }
+            }
+        )
+
+    }
+
+    /* Remove product by receiving the product id from the client and removing it in mongodb */
+    async removeProduct(request, response) {
+        response.send(request.body);
+        console.log("POST SUCCESS! Removed Product's ID: " + JSON.stringify(request.body));
+        console.log(request.body.id);
+        console.log(request.body.cat);
+        let prodID = request.body.id;
+        await Category.updateMany(
+            {},
+            { $pull: { products: { $in: [prodID] } } }
+        )
+        await Product.deleteMany(
+            { _id: prodID }
+        )
+    }
+
+    /* Get product information by client sending requested product id to server. Server can then access mongodb and get product */
+    async productById(request, response) {
+        const id = request.params.id;
+        console.log("ID: " + request.params.id);
+        const product = await Product.find({ _id: id })
+        console.log(product[0])
+        response.json({
+            id: product[0]._id,
+            title: product[0].title,
+            description: product[0].des,
+            price: product[0].price,
+            image: product[0].image
+        })
+        //console.log(response);
+    }
+
+    /* Edit product information by client sending new changes as a product object to update product in mongodb */
+    async editProduct(request, response) {
+        response.send(request.body);
+        console.log("POST SUCCESS! Edit Product! Can update mongo!");
+        console.log(request.body);
         console.log(request.body.id);
         console.log(request.body.title);
         console.log(request.body.description);
         console.log(request.body.price);
         console.log(request.body.image);
-        // request.body.category
-
-        var store = await this.getStore();
-
-        let product = await store.createProduct(
-            request.body.title,
-            request.body.description,
-            request.body.price,
-            request.body.image
+        // _id, title, des, price, image
+        let edit = await Product.updateMany(
+            { _id: request.body.id },
+            {
+                $set: {
+                    title: request.body.title,
+                    des: request.body.description,
+                    price: request.body.price,
+                    image: request.body.image
+                }
+            }
         )
-
-        // current category you are viewing
-
-        /*let products = {
-            0: {
-                id: 0,
-                title: "Katana",
-                description: "A very very sharp sword used by samurai",
-                price: 5.0,
-                image: "./assets/katana.png"
-            },
-            1: {
-                id: 1,
-                title: "Great Sword",
-                description: "A great sword, used by the largest men",
-                price: 25.0,
-                image: "./assets/greatsword.png"
-            },
-        }
-        products[Number(request.body.id)] = request.body;*/
-        //console.log("APPENDED");
-        //console.log(products);
-    }
-
-    /* Can rearrange layout/order of products by sending an array that contains the order based on the products' id */
-    rearrangeLayout(request, response) {
-        response.send(request.body);
-        console.log("POST SUCCESS! Rearrange Layout! Can update mongo!");
-        console.log(request.body);
-
-    }
-
-    /* Remove product by receiving the product id from the client and removing it in mongodb */
-    removeProduct(request, response) {
-        response.send(request.body);
-        console.log("POST SUCCESS! Removed Product's ID: " + JSON.stringify(request.body));
-
-
-    }
-
-    /* Get product information by client sending requested product id to server. Server can then access mongodb and get product */
-    productById(request, response) {
-        const id = request.params.id;
-        console.log("ID: " + request.params.id);
-        response.json({
-            id: id,
-            title: "Great Sword - EDIT",
-            description: "A great sword, used by the largest men. GETTING FOR EDIT",
-            price: 25.0,
-            image: "./assets/greatsword.png"
-        })
-        console.log(response);
-    }
-
-    /* Edit product information by client sending new changes as a product object to update product in mongodb */
-    editProduct(request, response) {
-        response.send(request.body);
-        console.log("POST SUCCESS! Edit Product! Can update mongo!");
-        console.log(request.body);
+        console.log(edit);
     }
 
     /*^^^^ ADMINISTRATION CODE ^^^^*/
