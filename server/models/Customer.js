edf39915649933670b1ff0d3d2e2d98ffdc6e9f5
dpaceoffice <dpaceoffice@gmail.com>
2022-06-13 22:03:20 -0500

    Additional parameters for customers

--- server/models/Customer.js
+++ server/models/Customer.js
@@ -1,14 +1,26 @@
 import mongoose from "mongoose";
 import Cart from "./Cart.js";
+import Store from "./Store.js";
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
   name: String,
   email: String,
   password: String,
   rights: Number,
+  phone: {
+    type: String,
+    default: '(000) 000-0000'
+  },
+  address: {
+    type: String,
+    default: 'ST, CITY, STATE'
+  },
   carts: [{ type: Schema.ObjectId, ref: 'Cart' }],
   curCart: Number,
   curCtg: Number,
+  profileImage: String,
+  purchases: [{ type: Schema.ObjectId, ref: 'Product' }],
+  purchase_quantities: { type: Map, of: Number, default: {} },
   htmlElement: String//`<i class="bi bi-house"></i> Greetings ${this.getName()}!`
 });
 
@@ -18,6 +30,42 @@ const userSchema = new Schema({
 class Customer {
   getCarts() {
     return this.carts;
+  }
+  async setProfileImage(image) {
+    this.profileImage = image;
+    await this.save();
+  }
+  async setName(name) {
+    this.name = name;
+    await this.save();
+  }
+  async setAddress(address) {
+    this.address = address;
+    await this.save();
+  }
+  async setPurchases(purchases) {
+    this.purchases = purchases;
+    await this.save();
+  }
+  async addPurchases(purchases, quantities) {
+    for (let i in purchases) {
+      const id = purchases[i];
+      const product = await Store.findProduct(id);
+      if (!this.purchase_quantities.has(id.toString())) {
+        this.purchases.push(product);
+        this.purchase_quantities.set(id, quantities.get(id));
+      } else
+        this.purchase_quantities.set(id, this.purchase_quantities.get(id) + quantities.get(id));
+    }
+    await this.save();
+  }
+  async setPhone(phone) {
+    this.phone = phone;
+    await this.save();
+  }
+  async setEmail(email) {
+    this.email = email;
+    await this.save();
   }
   /**
    * Gets the cart's id
