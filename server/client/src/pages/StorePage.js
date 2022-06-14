8fd0eeff0152d909683c7f9837f85473d6e0ad1b
dpaceoffice <dpaceoffice@gmail.com>
2022-06-13 22:00:43 -0500

    Maintains order of category

--- server/client/src/pages/StorePage.js
+++ server/client/src/pages/StorePage.js
@@ -10,21 +10,39 @@ export default class StorePage extends Component {
     render() {
         let active = undefined;
         const categories = this.props.data.cstate;
+        let cat_order = categories[this.props.data.start].order;
+        if (cat_order.length <= 0) {
+            cat_order = undefined;
+        }
         const products = this.props.data.pstate;
         let category_inner = [];
         let products_inner = [];
-        for (const category in categories) {
-            const data = categories[category];
-            const id = data['_id'];
-            const type = data['type'];
+        let populate = (data, id, type) => {
             if (this.state.ctg == undefined)
                 this.state.ctg = id;
             if (id === this.state.ctg)
                 active = data['products']
             category_inner.push(<li key={id} onClick={() => { this.setState({ ctg: id }) }} className="btn list-group-item">{type}</li>);
         }
+        if (cat_order == undefined)
+            for (const category in category) {
+                const data = categories[category];
+                const id = data['_id'];
+                const type = data['type'];
+                populate(data, id, type);
+            }
+        else
+            for (const i in cat_order) {
+                const category = cat_order[i];
+                const data = categories[category];
+                const id = data['_id'];
+                const type = data['type'];
+                populate(data, id, type)
+            }
         for (const index in active) {
             let product = active[index];
+            if (product == null)
+                continue;
             let data = products[product];
             var id = data['_id'];
             var title = data['title'];
@@ -38,7 +56,7 @@ export default class StorePage extends Component {
         }
 
         let html = <div><div className="row mt-5 text-center">
-            <div className="col">
+            <div className="col-sm" style={{ maxWidth: '18rem', marginLeft: '60px' }}>
                 <div className="card mb-2" style={{ maxWidth: '18rem', marginLeft: '60px' }}>
                     <ul className="list-group list-group-flush">
                         <div id="categories-container">
@@ -47,7 +65,7 @@ export default class StorePage extends Component {
                     </ul>
                 </div>
             </div>
-            <div className="col">
+            <div className="col-sm">
                 <div id="products-container" className="row ms-5">
                     {products_inner}
                 </div>
